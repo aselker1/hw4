5818eea20a0d66f22832d96a759e9bf53b0ac920
aselker1 <mamatovaaselya28@gmail.com>
2023-11-13 22:05:46 +0600

    +dop dz

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [time, setTime] = useState(0);
-  const [aos, setAos] = useState(false);
+  const [isOn, setIsOn] = useState(false);
   const [milliseconds, setMilliseconds] = useState(0);
+  const [history, setHistory] = useState([]);
 
   useEffect(() => {
     let timer;
-    if (aos) {
+    if (isOn) {
       timer = setTimeout(() => {
         setMilliseconds((ms) => {
           if (ms + 100 >= 1000) {
@@ -25,20 +26,21 @@ function App() {
     return () => {
       clearTimeout(timer);
     };
-  }, [aos, milliseconds]);
+  }, [isOn, milliseconds]);
 
   const start = () => {
-    setAos(true);
+    setIsOn(true);
   };
 
   const pause = () => {
-    setAos((aos) => !aos);
+    setIsOn((isOn) => !isOn);
   };
 
   const reset = () => {
+    setHistory([...history, formatTime(time, milliseconds)]);
     setTime(0);
     setMilliseconds(0);
-    setAos(false);
+    setIsOn(false);
   };
 
   const formatTime = (totalSeconds, ms) => {
@@ -52,13 +54,20 @@ function App() {
 
   return (
     <div className="App">
-      
+      <h1>Секундомер</h1>
+      <br />
       <h1>{formatTime(time, milliseconds)}</h1>
       <div className='btns'>
         <button id="start" onClick={start}>Start</button>
         <button id="pause" onClick={pause}>Pause</button>
         <button id="reset" onClick={reset}>Reset</button>
       </div>
+      <div>History</div>
+      <ul>
+        {history.map((entry, index) => (
+          <li key={index}>{entry}</li>
+        ))}
+      </ul>
     </div>
   );
 }
